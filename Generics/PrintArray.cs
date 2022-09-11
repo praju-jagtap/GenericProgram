5e3c5d5044212f6d912307450bb85b5d3564817a
praju-jagtap <prajaktajagtap2412@gmail.com>
2022-09-11 13:08:08 +0530

    [Prajakta] Add: To Print ArrayList

---
+++ Generics/PrintArray.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    internal class PrintArray             
+    {
+        public void createArray()         
+        {
+            int[] intArr = { 1, 2, 3, 4, 5, 6 };
+            double[] doubleArr = { 0.1, 0.2, 0.3, 0.4, 0.5 };
+            char[] charArr = { 'a', 'b', 'c', 'd', 'e', 'f' };
+
+            PrintIntArr(intArr);              
+            PrintDoubleArr(doubleArr);
+            PrintCharArr(charArr);
+        }
+        static void PrintIntArr(int[] arr)
+        {
+            foreach (var data in arr)
+            {
+                Console.WriteLine(data);
+            }
+        }
+        static void PrintDoubleArr(double[] arr)
+        {
+            foreach (var data in arr)
+            {
+                Console.WriteLine(data);
+            }
+        }
+        static void PrintCharArr(char[] arr)
+        {
+            foreach (var data in arr)
+            {
+                Console.WriteLine(data);
+            }
+        }
+    }
+}
--- Generics/Program.cs
+++ Generics/Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome To Generics Program");
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("1.Print Array\n");
+                Console.WriteLine("Please Enter Your Choice : ");
+                int option = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("---------------------------------\n");
+                switch (option)
+                {
+                    case 1:
+                        PrintArray printArray = new PrintArray();
+                        printArray.createArray();
+                        break;
+
+                    default:
+                        Console.WriteLine("Please Enter Valid Choice");
+                        break;
+                }
+            }
         }
     }
 }
