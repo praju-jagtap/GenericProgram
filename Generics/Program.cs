22bab7ffff33b3029416eef81c98c2f54753a39f
praju-jagtap <prajaktajagtap2412@gmail.com>
2022-09-11 13:20:42 +0530

    [Prajakta] Add: Using Generic To Print ArrayList

--- Generics/Program.cs
+++ Generics/Program.cs
@@ -7,15 +7,19 @@
             bool flag = true;
             while (flag)
             {
-                Console.WriteLine("1.Print Array\n");
+                Console.WriteLine("1.Print Array\n2.Using Generic Print Array\n");
                 Console.WriteLine("Please Enter Your Choice : ");
                 int option = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("---------------------------------\n");
+                Console.WriteLine("------------------------------------\n");
                 switch (option)
                 {
                     case 1:
                         PrintArray printArray = new PrintArray();
                         printArray.createArray();
+                        break;
+                    case 2:
+                        UsingGenericArray usingGenericArray = new UsingGenericArray();
+                        usingGenericArray.usingGeneric();
                         break;
 
                     default:
---
+++ Generics/UsingGenericArray.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    internal class UsingGenericArray
+    {
+        public void usingGeneric()
+        {
+            int[] intArr = {1, 2, 3, 4, 5};
+            double[] doubleArr = {1.1, 2.2, 3.3, 4.4, 5.5};
+            char[] charArr = {'a', 'b', 'c', 'd', 'e'};
+            PrintArr<int>(intArr);
+            PrintArr<double>(doubleArr);
+            PrintArr<char>(charArr);
+        }
+        static void PrintArr<T>(T[] arr)
+        {
+            foreach (var data in arr)
+            {
+                Console.WriteLine(data);
+            }
+        }
+    }
+}
